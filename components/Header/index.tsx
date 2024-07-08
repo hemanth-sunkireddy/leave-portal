6b4a102661ee10ebdc790121b63d63ccdcf95d00
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-07-08 09:48:22 +0530

    added theme support

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -153,7 +153,13 @@ const Header = () => {
                   </ul>
                 </nav>
               </div>
+              <div className="flex items-center justify-end pr-16 lg:pr-0">
+                <div>
+                  <ThemeToggler />
+                </div>
+              </div>
             </div>
+
           </div>
         </div>
       </header>
