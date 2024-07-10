c2d8c69da401159f76a8160a341a359c8ecf86f9
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-07-10 12:01:01 +0530

    added user type

--- components/Signup/index.tsx
+++ components/Signup/index.tsx
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 
 const SignupForm = () => {
-  // const [selectedUserType, setSelectedUserType] = useState('');
+  const [selectedUserType, setSelectedUserType] = useState('');
   const [name, setName] = useState('');
   const [pin, setPin] = useState('');
   const [password, setPassword] = useState('');
@@ -28,9 +28,9 @@ const SignupForm = () => {
 
   const db = getFirestore(app);
 
-  // const handleUserTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-  //   setSelectedUserType(event.target.value);
-  // };
+  const handleUserTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedUserType(event.target.value);
+  };
 
   const generateRandomString = (length) => {
     let result = '';
@@ -81,7 +81,7 @@ const SignupForm = () => {
         Name: name,
         Pin: pin,
         Password: password,
-        UserType: "Student",
+        UserType: selectedUserType,
         SignUpTime: isoString,
         UsageCount: 1,
         UniqueID: randomString
@@ -184,7 +184,7 @@ const SignupForm = () => {
                     className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                   />
                 </div>
-                {/* <div className="mb-8">
+                <div className="mb-8">
                   <label htmlFor="userType" className="block text-sm text-dark dark:text-white mb-3">
                     Select User Type
                   </label>
@@ -196,17 +196,15 @@ const SignupForm = () => {
                     className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                   >
                     <option value="">Select...</option>
-                    <option value="student">Student</option>
-                    <option value="mentor">Mentor</option>
-                    <option value="warden">Warden</option>
-                    <option value="principal">Principal</option>
+                    <option value="Student">Student</option>
+                    <option value="Facutly">Faculty</option>
                   </select>
                   {selectedUserType && (
                     <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
                       You selected: {selectedUserType}
                     </p>
                   )}
-                </div> */}
+                </div>
 
                 <div className="mb-6">
                   <button type="submit" className="shadow-submit dark:shadow-submit-dark flex w-full items-center justify-center rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90">
